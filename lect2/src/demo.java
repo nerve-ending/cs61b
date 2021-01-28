a270c4924b1ceda7eb4ca162abb2a45e161a4f49
赵丁立 <zhaodingli@zhaodinglideMacBook-Pro.local>
2021-01-29 02:07:58 +0800

    plan1

--- lect2/src/demo.java
+++ lect2/src/demo.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class demo {
+    /*
+    * Print all primes up to ARGS[0] (interpreted as an
+    *  integer), 10 to a line
+    * */
+    public static void main(String[] args) {
+        printPrimes(Integer.parseInt(args[0]));
+    }
+
+    /*
+    * Print all primes up to and including LIMIT,
+    * 10 to a line
+    * */
+    private static void printPrimes(int limit) {
+        /*{ For every integer, x, between 2 and LIMIT, print
+            isPrime(x), 10 to a line.} */
+    }
+
+    /* True iff X is prime */
+    private static boolean isPrime(int x) {
+        return /*(X is prime)*/
+    }
 }
