5df094c889b23c22aa0f01d474b0790ecd6d9405
nerve-ending <zhaodingli@qq.com>
2021-02-17 18:05:34 +0800

    demoDebug

--- src/debugging/averageFinder.java
+++ src/debugging/averageFinder.java
@@ -1,5 +1,17 @@
 package debugging;
 
-public class averegeFinder {
-    
+public class averageFinder {
+    public static void main(String[] args) {
+        System.out.println("Average finder v1.0 ");
+        double avg = findAverage(args);
+        System.out.println("The average is " + avg);
+    }
+
+    public static double findAverage(String[] input) {
+        double result = 0;
+        for (String s : input) {
+            result += Integer.parseInt(s);
+        }
+        return result / input.length;
+    }
 }
